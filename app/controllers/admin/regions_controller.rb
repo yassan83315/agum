cfa27ed942a6f2f10fd11509d44f9ba4744de772
Ubuntu <ubuntu@ip-172-31-39-52.ap-northeast-1.compute.internal>
2019-08-24 10:30:22 +0000

    [Add]地域情報の新規追加と編集の追加

--- app/controllers/admin/regions_controller.rb
+++ app/controllers/admin/regions_controller.rb
@@ -1,15 +1,38 @@
 class Admin::RegionsController < ApplicationController
   def new
+    @region = Region.new
   end
 
   def edit
+    @region = Region.find(params[:id])
   end
 
   def create
+    @region = Region.new(region_params)
+      if @region.save
+        flash[:notice] = "項目の登録に成功しました"
+      else
+        flash[:notice] = "項目の登録に失敗しました"
+      end
+    redirect_to new_admin_region_path
   end
 
   def update
+    @region = Region.find(params[:id])
+      if @region.update(region_params)
+        flash[:notice] = "項目内容を変更しました"
+        redirect_to admin_posts_path
+      else
+        flash[:alert] = "項目内容の変更に失敗しました"
+        redirect_to edit_admin_region_path
+      end
   end
 
-  def destroy
+
+  private
+
+  def region_params
+    params.require(:region).permit(:region_name)
   end
+
+end
