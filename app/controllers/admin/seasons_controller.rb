2bac34c10c1da2c335b663fa2e9bce9065e0121e
Ubuntu <ubuntu@ip-172-31-39-52.ap-northeast-1.compute.internal>
2019-08-24 10:51:37 +0000

    [Add]季節の新規追加と編集機能の追加

--- app/controllers/admin/seasons_controller.rb
+++ app/controllers/admin/seasons_controller.rb
@@ -1,16 +1,37 @@
 class Admin::SeasonsController < ApplicationController
   def new
+    @season = Season.new
   end
 
   def edit
+    @season = Season.find(params[:id])
   end
 
   def create
+    @season = Season.new(season_params)
+      if @season.save
+        flash[:notice] = "項目の登録に成功しました"
+      else
+        flash[:alert] = "項目の登録に失敗しました"
+      end
+    redirect_to new_admin_season_path
   end
 
   def update
+    @season = Season.find(params[:id])
+    if @season.update(season_params)
+      flash[:notice] = "項目内容の変更に成功しました"
+      redirect_to admin_posts_path
+    else
+      flash[:alert] = "項目内容の変更に失敗しました"
+      redirect_to edit_admin_season_path
+    end
   end
 
-  def destroy
+  private
+
+  def season_params
+    params.require(:season).permit(:season_name)
   end
+
 end
